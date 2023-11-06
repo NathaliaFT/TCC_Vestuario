9f619c47e4a81e70f5624a1a80611a5ee1b21080
guilhermeazx <guiassishg11@gmail.com>
2023-11-06 19:52:39 -0300

    criação do  about.html

--- tcc/setup/sitecatalogo/urls.py
+++ tcc/setup/sitecatalogo/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("registro", views.registro_usuario, name="registro"),
     path("catalogo_usuario", views.catalogo, name="catalogo_usuario"),
     path("user_login", views.user_login, name="user_login"),
